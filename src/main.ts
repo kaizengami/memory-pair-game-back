fc6f20db525a09af4c1881704b02e4e31c29c595
Oleksandr Semeniuta <olexander.sj@gmail.com>
2019-09-16 14:11:57 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,15 @@ import * as config from 'config';
 async function bootstrap() {
   const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, );
 
-  // if (process.env.NODE_ENV === 'development') {
-  //   app.enableCors();
-  // } else {
-  //   app.enableCors({ origin: serverConfig.origin });
-  //   logger.log(`Accepting requests from origin "${serverConfig.origin}"`);
-  // }
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    // app.enableCors({ origin: serverConfig.origin });
+    // logger.log(`Accepting requests from origin "${serverConfig.origin}"`);
+    app.enableCors();
+  }
 
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
